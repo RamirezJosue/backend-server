f02b977e1fa846c7e9216685d3ff4f6fd5ebaefe
RamirezJosue <xdemobtkx@gmail.com>
2018-03-29 12:17:10 -0600

    CRUD-Imagnes

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ app.use(bodyParser.json())
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
 var loginRoutes = require('./routes/login');
+var hospitalRoutes = require('./routes/hospital');
+var medicoRoutes = require('./routes/medico');
+var busquedaRoutes = require('./routes/busqueda');
+var uploadRoutes = require('./routes/upload');
+var imagenesRoutes = require('./routes/imagenes');
 
 
 // Conexión a la base de datos
@@ -28,14 +33,25 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
 });
 
+// Server index config
+// var serveIndex = require('serve-index');
+// app.use(express.static(__dirname + '/'))
+// app.use('/uploads', serveIndex(__dirname + '/uploads'));
+
 
 // Rutas
 app.use('/usuario', usuarioRoutes);
+app.use('/hospital', hospitalRoutes);
+app.use('/medico', medicoRoutes);
 app.use('/login', loginRoutes);
+app.use('/busqueda', busquedaRoutes);
+app.use('/upload', uploadRoutes);
+app.use('/img', imagenesRoutes);
+
 app.use('/', appRoutes);
 
 
 // Escuchar peticiones
-app.listen(000, () => {
+app.listen(8000, () => {
     console.log('Express server puerto 8000: \x1b[32m%s\x1b[0m', 'online');
 });

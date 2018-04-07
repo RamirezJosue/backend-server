70928236c05ea9c20098e4633819851b0736373f
RamirezJosue <xdemobtkx@gmail.com>
2018-04-07 13:05:47 -0500

    backend terminado

--- config/config.js
+++ config/config.js
@@ -1,1 +1,7 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+
+// Google
+
+module.exports.GOOGLE_CLIENT_ID = '855319730912-75ftmlkgm6rpf7ngtsp7ndkm3iic8ab2.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'HPbfSSewCogJDuhvHE9Ee50u';
